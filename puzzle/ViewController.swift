e2a6a874ae7c6c7a5b42f19358357f96420f5d0b
Marvin Nguyen <cafemarvin@146verdi.com>
2015-03-07 22:47:24 -0800

    Initial Commit

---
+++ puzzle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  puzzle
+//
+//  Created by Marvin N on 2015-03-07.
+//  Copyright (c) 2015 Marvin Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
